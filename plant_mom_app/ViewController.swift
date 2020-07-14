2fdb1a6bebed7fa9e38e45f6e6af1640558d73fe
Xenia Capcan <xcapcan@gmail.com>
2020-07-14 13:43:06 -0700

    first commit

---
+++ plant_mom_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plant_mom_app
+//
+//  Created by Olga Capcan on 7/14/20.
+//  Copyright © 2020 Xenia Capcan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
