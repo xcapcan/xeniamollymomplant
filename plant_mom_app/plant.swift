bc25b221a55a63c5268d0190492cba8d1306f7d2
Xenia Capcan <xcapcan@gmail.com>
2020-07-15 22:49:32 -0700

    complete iteration 0

---
+++ plant_mom_app/plant.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  plant_mom_app
+//
+//  Created by Olga Capcan on 7/15/20.
+//  Copyright © 2020 Xenia Capcan. All rights reserved.
+//
+
+import UIKit
+
+class plantBaby {
+    
+    var name = " "
+    var active = false
+    var type = " "
+    
+    
+}
